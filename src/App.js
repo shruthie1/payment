f20b87a8ee5972d78f35361526b4a64000c6ec01
sai <shettysaikumar3@gmail.com>
2021-01-17 18:56:08 +0530

    iu

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <form action="upi://pay?pa=bharatpe.0851610820@icici&amp;pn=Shruthi_reddy&amp;tn=Pay%20to%20Shruthi_Reddy&amp;cu=INR">
-    <input type="submit" value="Go to Google" />
-</form>
+      <a href="upi://pay?pa=bharatpe.0851610820@icici&amp;pn=Shruthi_reddy&amp;tn=Pay%20to%20Shruthi_Reddy&amp;cu=INR">click me</a>
       </header>
     </div>
   );
