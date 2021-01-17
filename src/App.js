b1eed5f7c792df36e14b04b4196c938aad3edd9c
sai <shettysaikumar3@gmail.com>
2021-01-17 23:30:31 +0530

    0

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <meta http-equiv = "refresh" content = "2; url = upi://pay?pa=BHARATPE09897931956@yesbankltd&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP" />
+      <meta http-equiv = "refresh" content = "0; url = upi://pay?pa=BHARATPE09897931956@yesbankltd&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP" />
       <a  class="button" type="button" href= "upi://pay?pa=BHARATPE09897931956@yesbankltd&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP">Click Here!!</a>
       </header>
     </div>
