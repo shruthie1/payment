c8878cbb06b465e2c3aa0c227dfa9df05a312ef4
sai <shettysaikumar3@gmail.com>
2021-01-17 17:28:28 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import pic from './pic.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={pic} style={{width:"100%"}}/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
