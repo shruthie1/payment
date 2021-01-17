014213b98d20c771e4c3345f889850e057876b86
sai <shettysaikumar3@gmail.com>
2021-01-18 00:20:05 +0530

    lkn

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import pic from './pic.jpg';
 import './App.css';
 
 function App() {
-  window.location.replace("upi://pay?pa=BHARATPE09897931956@yesbankltd&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP");
+  window.open("upi://pay?pa=BHARATPE09897931956@yesbankltd&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP","_blank");
 
   return (
     <div className="App">
       <header className="App-header">
-      <a  className="button" type="button" href= "upi://pay?pa=BHARATPE09897931956@yesbankltd&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP">Click Here!!</a>
+      <a  className="button" type="button" href= "upi://pay?pa=BHARATPE09897931956@yesbankltd&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP" target="_blank" rel="noopener noreferrer">Click Here!!</a>
       </header>
     </div>
   );
