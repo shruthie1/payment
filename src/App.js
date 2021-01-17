673eb29a062f0f12667c579a50a11f40e8d82e3c
sai <shettysaikumar3@gmail.com>
2021-01-17 17:36:15 +0530

    uyf

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={pic} style={{width:"100%"}}/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
