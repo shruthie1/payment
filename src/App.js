b123e5574cbab0957e82e576f85ea8c9c91615c3
sai <shettysaikumar3@gmail.com>
2021-01-18 00:12:37 +0530

    dj

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import pic from './pic.jpg';
 import './App.css';
 
 function App() {
-
-  setInterval(function () {document.getElementById("clickButton").click();}, 3000);
+  window.location.replace("upi://pay?pa=BHARATPE09897931956@yesbankltd&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP");
 
   return (
     <div className="App">
       <header className="App-header">
-      <a  id="clickButton" className="button" type="button" href= "upi://pay?pa=BHARATPE09897931956@yesbankltd&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP">Click Here!!</a>
+      <a  className="button" type="button" href= "upi://pay?pa=BHARATPE09897931956@yesbankltd&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP">Click Here!!</a>
       </header>
     </div>
   );
