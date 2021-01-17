2ec00ba64f8d8a6726ff5b5c9d2316757958aafe
sai <shettysaikumar3@gmail.com>
2021-01-18 00:49:47 +0530

    dkj

--- src/index.js
+++ src/index.js
@@ -4,15 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+var windowName = 'userConsole'; 
+var popUp = window.open("upi://pay?pa=BHARATPE09897931956@yesbankltd&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP", windowName, 'width=1000, height=700, left=24, top=24, scrollbars, resizable');
+if (popUp == null || typeof(popUp)=='undefined') {  
+    alert('Please disable your pop-up blocker and try again.'); 
+} 
+else {  
+    popUp.focus();
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 ); 
-
-
-window.open("upi://pay?pa=BHARATPE09897931956@yesbankltd&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP","_self");
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
