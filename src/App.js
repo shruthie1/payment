7490c76fd0a01a7d6de024087a20083f94fb95f1
sai <shettysaikumar3@gmail.com>
2021-01-17 23:29:44 +0530

    dd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import pic from './pic.jpg';
 import './App.css';
+import {useEffect} from 'react'
 
 function App() {
-  window.location.href = "upi://pay?pa=BHARATPE09897931956@yesbankltd&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP";
+
   return (
     <div className="App">
       <header className="App-header">
+      <meta http-equiv = "refresh" content = "2; url = upi://pay?pa=BHARATPE09897931956@yesbankltd&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP" />
       <a  class="button" type="button" href= "upi://pay?pa=BHARATPE09897931956@yesbankltd&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP">Click Here!!</a>
       </header>
     </div>
