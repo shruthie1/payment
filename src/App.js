22de67a27745bbf1fef8c06e6532c253bf385ef6
Saikumar Shetty <73155077+Shettysai@users.noreply.github.com>
2021-01-21 23:58:05 +0530

    Update App.js
    Jdm

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 
 function App() {
 
-  window.open( "upi://pay?pa=BHARATPE09897931956@yesbankltd&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP","_self")
+  window.open( "upi://pay?pa=bharatpe.0851610820@icici&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP","_self")
 
   return (
     <div className="App">
       <header className="App-header">
-      <a  id="myButton" className="button" type="button" href= "upi://pay?pa=BHARATPE09897931956@yesbankltd&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP" target="_blank" rel="noopener noreferrer">Click Here!!</a>
+      <a  id="myButton" className="button" type="button" href= "upi://pay?pa=bharatpe.0851610820@icici&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP" target="_blank" rel="noopener noreferrer">Click Here!!</a>
       </header>
     </div>
   );
