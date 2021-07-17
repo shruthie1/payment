09c98e315d13b0e936279073297c7c2774a05263
shruthie1 <reddyshruthie@gmail.com>
2021-07-17 13:57:47 +0000

    final

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import './App.css';
 
 function App() {
   setTimeout(()=>{
-   // window.open("upi://pay?pa=bharatpe.0851610820@icici&pn=Shruthi_Reddy&cu=INR&tn=Shruthi_Reddy&tr=APP","_self");
-   document.querySelector('.button').click()
-  },800)
+    window.open("upi://pay?pa=bharatpe.0851610820@icici&pn=Shruthi_Reddy&cu=INR&tn=Shruthi_Reddy&tr=APP","_self");
+  },550)
   return (
     <div className="App">
       <header className="App-header">
