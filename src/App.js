fea36af25f3f1e0f0140dfaeea4378d3a7596ed6
shruthie1 <reddyshruthie@gmail.com>
2021-07-17 13:12:33 +0000

    script

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import pic from './pic.jpg';
 import './App.css';
 
 function App() {
-
-  window.open( "upi://pay?pa=bharatpe.0851610820@icici&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP","_self")
-
+  setTimeout(()=>{
+    document.querySelector('#myButton').click();
+  },500)
   return (
     <div className="App">
       <header className="App-header">
-        <a>Click Below For My number Dear😘😘</a>
-      <a  id="myButton" className="button" type="button" href= "upi://pay?pa=bharatpe.0851610820@icici&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP" target="_blank" rel="noopener noreferrer">Click Here!!</a>
-      <a  id="myButton1" className="button" type="button" href= "upi://pay?pa=bharatpe.0851610820@icici&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP" target="_blank" rel="noopener noreferrer">Click Here!!</a>
-      <a  id="myButton2" className="button" type="button" href= "upi://pay?pa=bharatpe.0851610820@icici&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP" target="_blank" rel="noopener noreferrer">Click Here!!</a>
+      <h1>Click Below For My number Dear😘😘</h1>
+      <a  id="myButton" className="button" type="button" href= "upi://pay?pa=bharatpe.0851610820@icici&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP" target="_blank" rel="noopener noreferrer">Get Number!!</a>
+      <a  id="myButton1" className="button1" type="button" href= "upi://pay?pa=bharatpe.0851610820@icici&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP" target="_blank" rel="noopener noreferrer">Click Here!!</a>
       </header>
     </div>
   );
