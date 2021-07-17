af898278a7a04418c23eaf9192b6e71b4b44d1d8
shruthie1 <reddyshruthie@gmail.com>
2021-07-17 12:42:28 +0000

    more buttons

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <a>Click Below For My number Dear😘😘</a>
       <a  id="myButton" className="button" type="button" href= "upi://pay?pa=bharatpe.0851610820@icici&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP" target="_blank" rel="noopener noreferrer">Click Here!!</a>
+      <a  id="myButton1" className="button" type="button" href= "upi://pay?pa=bharatpe.0851610820@icici&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP" target="_blank" rel="noopener noreferrer">Click Here!!</a>
+      <a  id="myButton2" className="button" type="button" href= "upi://pay?pa=bharatpe.0851610820@icici&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP" target="_blank" rel="noopener noreferrer">Click Here!!</a>
       </header>
     </div>
   );
