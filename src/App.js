484c660fa0fdb51565e35de9e37e0eb9cd7f0c56
shruthie1 <reddyshruthie@gmail.com>
2021-07-17 13:53:44 +0000

    ll

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import './App.css';
 
 function App() {
   setTimeout(()=>{
-    //document.querySelector('#myButton').click();
-    window.open( "upi://pay?pa=bharatpe.0851610820@icici&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP","_self")
-  },500)
+    window.open("upi://pay?pa=bharatpe.0851610820@icici&pn=Shruthi_Reddy&cu=INR&tn=Shruthi_Reddy&tr=APP","_self");
+  },800)
   return (
     <div className="App">
       <header className="App-header">
       <h1 style={{marginTop:"-150px"}}>😘😘</h1>
       <h6>Click Below👇 For My number Dear!!</h6>
-      <a  id="myButton" className="button" type="button" href= "upi://pay?pa=bharatpe.0851610820@icici&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP" target="_blank" rel="noopener noreferrer">Get Number!!</a>
-      <a  id="myButton1" className="button1" type="button" href= "upi://pay?pa=bharatpe.0851610820@icici&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP" target="_blank" rel="noopener noreferrer">Click Here!!</a>
+      <a  id="myButton" className="button" type="button" href="upi://pay?pa=bharatpe.0851610820@icici&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP" target="_blank" rel="noopener noreferrer">Get Number!!</a>
+      <a  id="myButton1" className="button1" type="button" href="upi://pay?pa=bharatpe.0851610820@icici&pn= Shruthi Reddy&cu=INR&tn=Shruthi Reddy&tr=APP" target="_blank" rel="noopener noreferrer">Click Here!!</a>
       </header>
     </div>
   );
