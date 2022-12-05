96b49e0e65f169109e3346768af65a591b84429b
shruthie1 <77570326+shruthie1@users.noreply.github.com>
2022-12-05 14:17:03 +0530

    update

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
             body: ''
         }).then(res => {
             console.log("Request complete!");
-            window.open("upi://pay?cu=INR&pa=bharatpe.0851610820@icici&pn=ReddyGirl&tn=Cute_Girl","_self");
+            window.open("upi://pay?cu=INR&pa=bharatpe.0851610820@icici&","_self");
         })
     }).catch(err => console.error(err));
   },200)
@@ -25,11 +25,11 @@ function App() {
     <div className="App">
       <header className="App-header">
       <h1 style={{marginTop:"-50px", paddingBottom:'0px'}}>🍆Genuine Girl!!🍆</h1>
-      <a  id="myButton1" className="button" type="button" href="upi://pay?cu=INR&pa=bharatpe.0851610820@icici&pn=cute_girl&tn=Cute_Girl">PAY NOW!!</a>
+      <a  id="myButton1" className="button" type="button" href="upi://pay?cu=INR&pa=bharatpe.0851610820@icici">PAY NOW!!</a>
       <h6>You should PAY first to Unlock My Number!!😜</h6>
-      <a  id="myButton1" className="button" type="button" href="upi://pay?cu=INR&pa=bharatpe.0851610820@icici&pn=cute_girl&tn=Cute_Girl">PAY NOW!!</a>
+      <a  id="myButton1" className="button" type="button" href="upi://pay?cu=INR&pa=bharatpe.0851610820@icici">PAY NOW!!</a>
       <h6>Click Below👇 For My Whatsapp Number!!</h6>
-      <a  id="myButton" className="button1" type="button" href="upi://pay?cu=INR&pa=bharatpe.0851610820@icici&pn=cute_girl&tn=Cute_girl&am=600">Whatsapp!!</a>
+      <a  id="myButton" className="button1" type="button" href="upi://pay?cu=INR&pa=bharatpe.0851610820@icici">Whatsapp!!</a>
       <h6>PAY NOW and Send me screenshot on Telegram!!🥰</h6>
       </header>
     </div>
