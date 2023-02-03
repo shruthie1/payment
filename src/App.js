8b9db5e7668fda718705299fdb539d813351fdab
sai shetty <saikumar.shetty@jda.com>
2023-02-03 15:57:15 +0530

    cefde

--- src/App.js
+++ src/App.js
@@ -81,9 +81,9 @@ function App() {
       <header className="App-header">
         <h1 style={{ marginTop: '-60px', color: "#82ffa5" }}><img style={{ width: "200px" }} alt='' src='./logo.svg'></img></h1>
         <p style={{ fontSize: "20px", fontWeight: "bolder", margin: '15px' }}>Copy <p style={{ color: '#c9df3d', cursor: 'pointer', display: 'contents' }} onClick={async () => {
-          navigator.clipboard.writeText("paytmqr28100505010111o4jao8e1ay@paytm");
-        }}>UPI ID </p>or  <a style={{ color: '#c9df3d', cursor: 'pointer' }} href='upi://pay?pa=paytmqr28100505010111o4jao8e1ay@paytm' onClick={async () => {
-          navigator.clipboard.writeText("paytmqr28100505010111o4jao8e1ay@paytm");
+          navigator.clipboard.writeText("paytmqr281005050101jnirp1ueoe1y@paytm");
+        }}>UPI ID </p>or  <a style={{ color: '#c9df3d', cursor: 'pointer' }} href='upi://pay?pa=paytmqr281005050101jnirp1ueoe1y@paytm' onClick={async () => {
+          navigator.clipboard.writeText("paytmqr281005050101jnirp1ueoe1y@paytm");
         }}>Scan</a> the QR code!!</p>
         <CopyExample />
         {/* <MsgBtnCombo msg="You should PAY first to Unlock My Number!!😜" btnName="PAY NOW!!" handler={handlepayButton} err={true}></MsgBtnCombo> */}
@@ -126,15 +126,15 @@ function CopyExample() {
         {/* <p style={{ fontSize: "15px" }}>If "<span style={{ color: "red" }}>PAY NOW</span>" Button is Not Working!!</p> */}
         <p style={{ margin: "10px" }}> Pay to my QR Code 👇🏻👇🏻👇🏻</p>
       </div>
-      <img className='qr' title='paytmqr28100505010111o4jao8e1ay@paytm' style={{ marginTop: "6px" }} alt='' src='./QR.jpg'></img>
+      <img className='qr' title='paytmqr281005050101jnirp1ueoe1y@paytm' style={{ marginTop: "6px" }} alt='' src='./QR.jpg'></img>
       <img className='upi' style={{ marginBottom: "0px", width: "140px" }} alt='' src='./upilogo.png'></img>
       {<div style={{ display: "flex", padding: "12px", height: "50px" }}>
         <span style={{ fontWeight: "bold", marginBottom: "5px" }}>UPI: </span>
-        <input title='paytmqr28100505010111o4jao8e1ay@paytm' readOnly value={"PaytmQR281......@paytm"} style={{ fontSize: '17px', textOverflow: 'ellipsis', fontWeight: 'normal', cursor: 'copy' }}></input >
-        <button title='paytmqr28100505010111o4jao8e1ay@paytm' className='cpybutton' onClick={async () => {
-          navigator.clipboard.writeText("paytmqr28100505010111o4jao8e1ay@paytm");
+        <input title='paytmqr281005050101jnirp1ueoe1y@paytm' readOnly value={"PaytmQR281......@paytm"} style={{ fontSize: '17px', textOverflow: 'ellipsis', fontWeight: 'normal', cursor: 'copy' }}></input >
+        <button title='paytmqr281005050101jnirp1ueoe1y@paytm' className='cpybutton' onClick={async () => {
+          navigator.clipboard.writeText("paytmqr281005050101jnirp1ueoe1y@paytm");
           setTimeout(() => {
-            window.location.href = 'upi://pay?pa=paytmqr28100505010111o4jao8e1ay@paytm'
+            window.location.href = 'upi://pay?pa=paytmqr281005050101jnirp1ueoe1y@paytm'
           }, 2500);
           setIsCopyOpen(true); await sendUpdate('Copied');
         }}>Copy</button>
