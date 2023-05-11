8a6ac82d8ac7c8ebda3930d9d8226abb26136871
sai shetty <saikumar.shetty@jda.com>
2023-05-11 12:44:34 +0530

    okaish

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,5 +1,5 @@
 const path = require('path');
-const webpack = require('webpack');
+// const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js', // the entry point of your app
